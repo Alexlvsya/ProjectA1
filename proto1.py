e9f5fb3e5f45955d894b460b7159f5d1860d5e97
Alexlvsya <alexramca@icloud.com>
2026-02-17 12:21:11 -0600

    Adjust image width in proto1.py

--- proto1.py
+++ proto1.py
@@ -189,7 +189,7 @@ if selected_option == "About the Author":
     st.image(
         image_url, 
         caption="Alejandro Ramirez  - Author", 
-        width=300,  # Evita que la imagen se expanda a todo el ancho
+          # Evita que la imagen se expanda a todo el ancho
         width=120  # Ajusta este valor según el tamaño real de la imagen
     )
     
